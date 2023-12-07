279485016cfe330ef368d04c4856138301139ebd
Kevin Wilson <kevin@partcoffee.com>
2023-12-07 00:02:59 +0000

    06 / 02b - quadratic

--- 06/getPermutations.ts
+++ 06/getPermutations.ts
@@ -3,25 +3,32 @@ export type Race = {
   time: number;
 };
 
-const speedPerDuration = 1;
+function solveUsingQuadraticEquation(
+  raceTime: number,
+  speedPerDurationValue: number,
+  distance: number,
+) {
+  const discriminant =
+    Math.pow(-raceTime, 2) - 4 * speedPerDurationValue * (distance + 1);
 
-export function getPermutations(race: Race) {
-  let total = 0;
+  if (discriminant >= 0) {
+    const positiveRoot =
+      (raceTime + Math.sqrt(discriminant)) / (2 * speedPerDurationValue);
+    const negativeRoot =
+      (raceTime - Math.sqrt(discriminant)) / (2 * speedPerDurationValue);
 
-  for (
-    let timeButtonPressed = 0;
-    timeButtonPressed < race.time;
-    timeButtonPressed++
-  ) {
-    const speedGathered = timeButtonPressed * speedPerDuration;
-    const remainingTime = race.time - timeButtonPressed;
-
-    const distance = remainingTime * speedGathered;
-
-    if (distance > race.distance) {
-      total++;
-    }
+    return [positiveRoot, negativeRoot].filter((root) => root > 0);
   }
 
-  return total;
+  return [];
+}
+
+export function getPermutations(race: Race) {
+  const speedPerDuration = 1;
+  const [upper, lower] = solveUsingQuadraticEquation(
+    race.time,
+    speedPerDuration,
+    race.distance,
+  );
+  return Math.floor(upper) - Math.ceil(lower) + 1; // +1 because inclusive of the values e.g. [1,3] is 3 because [1,2,3]
 }
