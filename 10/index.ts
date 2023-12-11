06272b33a39ada87f59c7f6ebac801f82321f62b
Kevin Wilson <kevin@partcoffee.com>
2023-12-11 01:41:41 +0000

    10 / 02

--- 10/index.ts
+++ 10/index.ts
@@ -20,6 +20,8 @@ function parseData(lines: string[]): string[][] {
   return lines.map((line) => line.split(''));
 }
 
+const verticalPipes = /F-*J|L-*7|\|/g;
+
 async function main() {
   const map = parseData(await getData());
   const startPoint = getStartPoint(map);
@@ -31,19 +33,62 @@ async function main() {
     return nextPoints.filter((x) => !visitedPoints.has(x.toString())).at(0);
   }
 
-  const visitedPoints = new Set<string>();
+  const visitedPoints = new Map<string, { char: string }>();
   let nextPoint = getNextPoint();
 
   while (nextPoint) {
     currentPosition = nextPoint;
-    visitedPoints.add(currentPosition.toString());
+
+    const char = map[currentPosition[0]][currentPosition[1]];
+    visitedPoints.set(currentPosition.toString(), { char });
     nextPoint = getNextPoint();
+  }
+
+  // Look for enclosed areas
+  let enclosedCount = 0;
+
+  for (let rowIndex = 0; rowIndex < map.length; rowIndex++) {
+    const row = map[rowIndex];
+
+    for (let columnIndex = 0; columnIndex < row.length; columnIndex++) {
+      const currentPositionKey = [rowIndex, columnIndex].toString();
+      const isWall = visitedPoints.has(currentPositionKey);
+
+      // Ignore walls
+      if (isWall) {
+        continue;
+      }
+
+      // get all points from left to here
+      const pointsToHere = row
+        .slice(0, columnIndex + 1)
+        .map((_, i) => [rowIndex, i]);
+
+      // Then get the string path to here
+      const pathToHere = pointsToHere.map((point) => {
+        const value = visitedPoints.get(point.toString());
+        if (value) {
+          return value.char;
+        }
+
+        return '.';
+      });
+
+      // if points cross odd number of pipes, it's inside
+      // https://en.wikipedia.org/wiki/Even%E2%80%93odd_rule
+      const matches = [...pathToHere.join('').matchAll(verticalPipes)].length;
+
+      if (matches % 2 === 1) {
+        enclosedCount++;
+      }
+    }
   }
 
   console.log({
     currentPosition,
     count: visitedPoints.size,
     distance: visitedPoints.size / 2,
+    enclosedCount,
   });
 }
 
